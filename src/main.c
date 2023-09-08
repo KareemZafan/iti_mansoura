429129450903972306d39d1965decf0c96bc8b7a
KareemZafan <kareemm.zafan@gmail.com>
2023-09-08 16:06:14 +0300

    Adding new feature

--- src/main.c
+++ src/main.c
@@ -1,10 +1,13 @@
 #include <iosream.h>
-
-
+#incude <math.h> 
 
 int main (void){
 
  printf("Hello Together") ; 
+ int a = 1; 
+ int b = 15 ; 
+ 
+ printf("\nSquare Root of %d and %d = %d\n", a , b , sqrt(a + b)); 
 
 return 0 ; 
 }
